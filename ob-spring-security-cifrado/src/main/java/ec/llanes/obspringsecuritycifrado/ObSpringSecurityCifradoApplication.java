d8d89370e707b0493b6789d0a9d980cf7e45089b
jllanes <jllanes@baustro.fin.ec>
2022-05-07 12:48:28 -0500

    encript

---
+++ ob-spring-security-cifrado/src/main/java/ec/llanes/obspringsecuritycifrado/ObSpringSecurityCifradoApplication.java
@@ -0,0 +1,13 @@
+package ec.llanes.obspringsecuritycifrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringSecurityCifradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObSpringSecurityCifradoApplication.class, args);
+	}
+
+}
