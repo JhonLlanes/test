2cbedb2b97b3a01da25de22d76c22c19abe71213
jllanes <jllanes@baustro.fin.ec>
2022-05-11 16:25:51 -0500

    HTML

---
+++ obj-jwt-auth/src/main/java/com/llanes/objjwtauth/ObjJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.llanes.objjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjJwtAuthApplication.class, args);
+	}
+
+}
